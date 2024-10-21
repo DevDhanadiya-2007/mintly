377db19f6104d886f4c73e9bf9dd1ca815fef9a1
dev <devdhanadiya07@gmail.com>
2024-10-21 17:53:05 +0530

    Initialization:- Database and Alias

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        config.cache = {
+            type: 'filesystem',
+            cacheDirectory: path.resolve(__dirname, '.next/cache'),
+            buildDependencies: {
+                config: [__filename],
+            },
+        };
+
+        return config;
+    },
+};
 
 export default nextConfig;
