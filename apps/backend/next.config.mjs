363b2c5a6a38ab4b714de7d66ae42eee5b1987d0
dev <devdhanadiya07@gmail.com>
2024-10-22 17:56:31 +0530

    Task 1:- 1.1 Ui and Authentication updated the middleware configurations in next config

--- apps/backend/next.config.mjs
+++ apps/backend/next.config.mjs
@@ -10,11 +10,10 @@ const nextConfig = {
                     { key: 'Access-Control-Allow-Origin', value: 'http://localhost:3000' },
                     { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
                     { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-                ]
-            }
-        ]
+                ],
+            },
+        ];
     },
-    middleware: ['cors'], // Enable the CORS middleware
 };
 
 export default nextConfig;
